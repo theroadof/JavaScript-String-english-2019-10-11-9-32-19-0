064b38bb2fb54a20e1d93f339de7b74b93dc2deb
theroadof <1294911641@qq.com>
2020-06-10 15:17:19 +0800

    commit

---
+++ 1.js
@@ -0,0 +1,16 @@
+// Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
+var name = 'hello';
+// TODO write your code here...
+document.write(name.toUpperCase()+'<br>');
+
+
+// Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
+var sentence = 'good afternoon, mr mike.';
+//TODO write your code here...
+document.write(sentence.toUpperCase()+'<br>');
+
+
+// Complete the following procedure to extract the amount of money variable, output: 20.
+var money = '￥20';
+//TODO write your code here...
+document.write(money.substring(1,3));
